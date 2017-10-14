b744d94aedd4962eecfa87ae756b478a4bb6663e
pacts <ibenamos@gmail.com>
2017-10-14 20:51:13 +0000

    cleaning up and adding configurations

--- index.js
+++ index.js
@@ -3,35 +3,31 @@ const HOST = '192.168.43.196';
 
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
+const fs = require('fs');
 
 const makePwmDriver = require('adafruit-i2c-pwm-driver');
 const pwmDriver = makePwmDriver({address: 0x40, device: '/dev/i2c-1'});
 pwmDriver.setPWMFreq(60);
 
-const chanels = {
-    head : "0",
-    left_leg : "1",
-    right_leg: "2"
-}
+var chanels;
 
 var sleep = require('sleep');
 
 server.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
+    chanels = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 });
 
 server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
     var data = JSON.parse(message);
-    console.log(data);
+    if(data['robotSelect'] != undefined){
+        chanels = JSON.parse(fs.readFileSync('./configs/'+ data['robotSelect'] +'.json', 'utf8'));
+    }
     var keys = Object.keys(data);
     for(var i = 0; i < keys.length; i++) {
         if(chanels.hasOwnProperty(keys[i])){
             pwmDriver.setPWM(chanels[keys[i]], 0, data[keys[i]]);
-            console.log(keys[i]);
-            console.log(chanels[keys[i]]);
-            console.log(data[keys[i]]);
         }
     }
 });
