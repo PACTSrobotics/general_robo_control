63d0ebefc5fae32e13b62e7e0e58b4184b2df542
patrick woodburn <pat@patrickwoodburn.com>
2018-02-24 18:51:51 +0000

    setup for pi zeros

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const PORT = 33333;
-const HOST = '192.168.43.196';
+const HOST = '192.168.43.210';
 
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
@@ -20,6 +20,7 @@ server.on('listening', function () {
 });
 
 server.on('message', function (message, remote) {
+    console.log(message)
     var data = JSON.parse(message);
     if(data['robotSelect'] != undefined){
         chanels = JSON.parse(fs.readFileSync('./configs/'+ data['robotSelect'] +'.json', 'utf8'));
@@ -27,7 +28,7 @@ server.on('message', function (message, remote) {
     var keys = Object.keys(data);
     for(var i = 0; i < keys.length; i++) {
         if(chanels.hasOwnProperty(keys[i])){
-            pwmDriver.setPWM(int(chanels[keys[i]]), 0, int(data[keys[i]]));
+            pwmDriver.setPWM(parseInt(chanels[keys[i]]), 0, parseInt(data[keys[i]]));
         }
     }
 });
