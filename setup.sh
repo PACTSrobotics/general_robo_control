b24bac4f0a9b4ffc13364db52163768e4c8c4551
buzz <buzz627@gmail.com>
2019-06-29 16:31:44 +0100

    startup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt install supervisor
+git pull
+sudo ln -s /home/pi/general_robo_control/general_robo_control.conf /etc/supervisor/conf.d/ 
+sudo pip3 install -r requirements.txt
+sudo supervisorctl reread
+sudo supervisorctl update 
