77cb492f750bca71fee9ddeda11f7ff87be289f3
Buzz <buzz627@gmail.com>
2019-06-29 17:05:46 +0100

    created dalekHead. Switched update and reread

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ sudo apt install supervisor
 git pull
 sudo ln -s /home/pi/general_robo_control/general_robo_control.conf /etc/supervisor/conf.d/ 
 sudo pip3 install -r requirements.txt
-sudo supervisorctl reread
-sudo supervisorctl update 
+sudo supervisorctl update
+sudo supervisorctl reread 
